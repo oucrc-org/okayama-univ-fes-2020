eefef9aa704ac00a6f7b9bcf38643f22886086f3
山田敬汰 <banira0428@gmail.com>
2020-08-30 23:32:06 +0900

    add: コンテンツ数を動的に変更できるようにする

---
+++ components/Content.jsx
@@ -0,0 +1,11 @@
+export default function Content({content}) {
+    return (
+        <li className="rounded shadow-md m-3 border-2">
+            <img className="mb-1" src="1080x720.png" alt=""/>
+            <div className="m-3">
+                <p>{content.title}</p>
+                <p className="text-sm">ジャンル：あ</p>
+            </div>
+        </li>
+    )
+}
--- pages/Index.jsx
+++ pages/Index.jsx
@@ -3,13 +3,31 @@ import MainVisual from "../components/MainVisual";
 import Notice from "../components/Notice";
 import Contents from "../components/Contents";
 
-export default function Index() {
+export default function Index({contents}) {
     return (
         <div>
             <Header/>
             <MainVisual/>
             <Notice/>
-            <Contents/>
+            <Contents contents={contents}/>
         </div>
     )
+}
+
+export async function getStaticProps() {
+
+    return {
+        props: {
+            contents: [
+                {
+                    "id": 1,
+                    "title": "1つ目のタイトルです"
+                },
+                {
+                    "id": 2,
+                    "title": "2つ目のタイトルです"
+                }
+            ]
+        }
+    }
 }
