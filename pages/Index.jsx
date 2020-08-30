bafec81dfd1b11bfc88461b7130dc9a1d8b208b9
山田敬汰 <banira0428@gmail.com>
2020-08-30 22:49:40 +0900

    remove: 不要箇所の削除

--- pages/Index.jsx
+++ pages/Index.jsx
@@ -8,30 +8,6 @@ export default function Index() {
             <Header/>
             <MainVisual/>
             <Notice/>
-            <div className="hero">
-                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
-                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
-            </div>
-            <div className="hero">
-                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
-                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
-            </div>
-            <div className="hero">
-                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
-                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
-            </div>
-            <div className="hero">
-                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
-                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
-            </div>
-            <div className="hero">
-                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
-                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
-            </div>
-            <div className="hero">
-                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
-                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
-            </div>
         </div>
     )
 }
