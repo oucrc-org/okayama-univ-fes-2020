df3924afb4b4f091e495d1336edabae4421bd28b
山田敬汰 <banira0428@gmail.com>
2020-08-30 22:05:40 +0900

    add: ヘッダーを作成

---
+++ components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-blue-400 p-5 sticky top-0">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <span className="font-semibold text-xl tracking-tight">ここにタイトルが入ります</span>
+            </div>
+        </nav>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,33 @@
+import Header from '.././components/header'
+
 export default function Index() {
-  return (
-      <div>
-        <div className="hero">
-          <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
-          <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
+    return (
+        <div>
+            <Header/>
+            <div className="hero">
+                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
+                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
+            </div>
+            <div className="hero">
+                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
+                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
+            </div>
+            <div className="hero">
+                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
+                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
+            </div>
+            <div className="hero">
+                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
+                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
+            </div>
+            <div className="hero">
+                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
+                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
+            </div>
+            <div className="hero">
+                <h1 className="title">Next.js + Tailwind CSS 🐼</h1>
+                <p className="text-center text-teal-500 text-2xl py-4">This is an Example.</p>
+            </div>
         </div>
-      </div>
-  )
+    )
 }
