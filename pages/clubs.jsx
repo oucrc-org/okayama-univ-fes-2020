8135f4d1b1f397e75a9fc3a6544bfe85cdbc2740
山田敬汰 <banira0428@gmail.com>
2020-09-04 23:47:49 +0900

    add: コンテンツ取得処理を js に逃す

--- pages/clubs.jsx
+++ pages/clubs.jsx
@@ -1,23 +1,12 @@
 import Layout from "../components/Layout";
 import Content from "../components/Content";
+import {getClubs} from "../lib/clubs";
 
 export default function Clubs() {
-
-    const clubs = [
-        {
-            id: "1",
-            title: "oucrc"
-        },
-        {
-            id: "2",
-            title: "サークル"
-        }
-    ];
-
     return (
         <Layout>
             <ul className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-                {clubs.map((club) => (
+                {getClubs().map((club) => (
                     <Content key={club.id} content={club}/>
                 ))}
             </ul>
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Message from "../components/Message";
 import Notices from "../components/Notices";
 import Contents from "../components/Contents";
 import Layout from "../components/Layout";
-import {getNotices} from "../lib/notices"
+import {getNotices} from "../lib/notices";
+import {getContents} from "../lib/contents";
 
 export default function Index({contents, notices}) {
     return (
@@ -24,24 +25,7 @@ export async function getStaticProps() {
 
     return {
         props: {
-            contents: [
-                {
-                    "id": 1,
-                    "title": "〇〇サークルの紹介"
-                },
-                {
-                    "id": 2,
-                    "title": "〇〇部の動画"
-                },
-                {
-                    "id": 3,
-                    "title": "〇〇サークルの紹介"
-                },
-                {
-                    "id": 4,
-                    "title": "〇〇部の動画"
-                }
-            ],
+            contents: getContents(),
             notices: getNotices()
         }
     }
