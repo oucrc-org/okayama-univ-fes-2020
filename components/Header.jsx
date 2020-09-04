82498720ac39becdc639fe6e2806e4bbc0574312
山田敬汰 <banira0428@gmail.com>
2020-09-04 13:19:30 +0900

    add: ヘッダにトップページへの動線を追加

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-blue-400 p-5 sticky top-0 shadow z-10">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
-                <span className="font-semibold text-xl tracking-tight">ここにタイトルが入ります</span>
+                <Link href="/">
+                    <a><span className="font-semibold text-xl tracking-tight">ここにタイトルが入ります</span></a>
+                </Link>
             </div>
         </nav>
     )
