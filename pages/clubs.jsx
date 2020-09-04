5a1d9b3774b6d182b35649fdcb5ac7fe498f0307
山田敬汰 <banira0428@gmail.com>
2020-09-04 22:12:01 +0900

    add: サークル一覧画面の作成

--- pages/clubs.jsx
+++ pages/clubs.jsx
@@ -1,9 +1,26 @@
 import Layout from "../components/Layout";
+import Content from "../components/Content";
 
 export default function Clubs() {
+
+    const clubs = [
+        {
+            id: "1",
+            title: "oucrc"
+        },
+        {
+            id: "2",
+            title: "サークル"
+        }
+    ];
+
     return (
         <Layout>
-            <p>部活動・サークル紹介</p>
+            <ul className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+                {clubs.map((club) => (
+                    <Content key={club.id} content={club}/>
+                ))}
+            </ul>
         </Layout>
     )
 }
