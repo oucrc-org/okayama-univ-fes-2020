29b30a51caa112f77e50a614f3f0f073b0577a9a
a-msy <m5s2y5a@gmail.com>
2020-09-04 14:39:46 +0900

    フッター作成しました

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 import './styles.css'
 import 'slick-carousel/slick/slick.css'
 import "slick-carousel/slick/slick-theme.css";
+import scss from "../scss/ruckus.scss";
 
 function _app({ Component, pageProps }) {
     return <Component {...pageProps} />
