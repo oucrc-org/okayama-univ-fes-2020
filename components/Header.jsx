df7b44cf926431c59e8d5f87fe9a8145c0a1bf76
smpny7 <symphonymode7@icloud.com>
2020-10-10 13:57:13 +0900

    :add: PC 版ではハンバーガーメニューを非表示に

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import React, {Component} from "react";
+import React, { Component } from "react";
 // https://stackoverflow.com/questions/44752189/how-to-add-font-awesome-to-next-js-project
 // https://qiita.com/jesuissuyaa/items/f9cb9343057de306ff25
 // https://www.npmjs.com/package/@fortawesome/react-fontawesome#features
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {config, library} from '@fortawesome/fontawesome-svg-core';
-import {fas} from '@fortawesome/free-solid-svg-icons';
-import {getNavigations} from "../lib/navigations";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { getNavigations } from "../lib/navigations";
 import Transition from "react-transition-group/cjs/Transition";
 
 config.autoAddCss = false;
@@ -16,7 +16,7 @@ export default class Header extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {isShowMenu: false};
+        this.state = { isShowMenu: false };
     }
 
     render() {
@@ -26,7 +26,7 @@ export default class Header extends Component {
                 <div className="flex items-center justify-between flex-wrap p-2 h-16 xl:h-20 bg-blue-900 text-white">
                     <Link href="/">
                         <a>
-                            <img className="object-contain w-8 mr-2" src="/symbol.png" alt="校章"/>
+                            <img className="object-contain w-8 mr-2" src="/symbol.png" alt="校章" />
                         </a>
                     </Link>
                     <Link href="/">
@@ -34,12 +34,23 @@ export default class Header extends Component {
                             <p className="font-serif ">大学祭</p>
                         </a>
                     </Link>
-                    <button className="focus:outline-none" onClick={() => {
+                    <ul className="justify-center ml-auto mr-auto text-left hidden lg:block">
+                        {
+                            getNavigations().map((nav) =>
+                                <li key={nav.id} className="p-4 text-sm xl:text-md inline-block">
+                                    <Link href={nav.link}>
+                                        <a>{nav.title}</a>
+                                    </Link>
+                                </li>
+                            )
+                        }
+                    </ul>
+                    <button className="focus:outline-none lg:hidden" onClick={() => {
                         this.setState({
                             isShowMenu: !this.state.isShowMenu
                         })
                     }}>
-                        <FontAwesomeIcon icon={['fas', 'bars']} width="40" className="p-3"/>
+                        <FontAwesomeIcon icon={['fas', 'bars']} width="40" className="p-3" />
                     </button>
                 </div>
                 <Transition
