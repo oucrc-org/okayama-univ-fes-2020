699c8928c09b23146803a925d0f01fd73745f882
smpny7 <symphonymode7@icloud.com>
2020-10-10 13:36:52 +0900

    :add: スポンサーデータの追記

--- pages/sponsors.jsx
+++ pages/sponsors.jsx
@@ -9,14 +9,14 @@ export default function Sponsors() {
                     <div className="relative mb-6">
                         <div className="z-10 absolute w-3/4 bg-blue-600 h-full"/>
                         <div className="z-20 relative content-center pl-6 pt-3 pb-3 ml-auto mr-auto">
-                            <h1 className="text-xl text-white mt-3 font-serif">協賛企業一覧</h1>
+                            <h1 className="text-xl text-white mt-3 font-serif">協賛企業一覧(五十音順)</h1>
                         </div>
                     </div>
                     <div className="p-6">
                         <ul>
                             {getSponsors().map((sponsor) =>
                                 <li key={sponsor.id}>
-                                    <p className="text-xl p-1">{sponsor.title}</p>
+                                    <p className="text-xl p-1">・{sponsor.title}</p>
                                 </li>
                             )}
                         </ul>
