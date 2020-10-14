b65cc725ad0e1cbaaf3f6808bcc65067c45bc411
山田敬汰 <banira0428@gmail.com>
2020-10-14 19:18:03 +0900

    :add: ピックアップ団体企画用のヘッダーを追加

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Contents from "../components/Contents";
 import Layout from "../components/Layout";
 import {getFixedContents} from "../lib/contents";
 import {getPickUpProject} from "../lib/projects";
+import Heading from "../components/common/Heading";
 
 export default function Index({fixedContents, pickupProject, notices}) {
     return (
@@ -13,6 +14,7 @@ export default function Index({fixedContents, pickupProject, notices}) {
                 <div className="max-w-3xl container ml-auto mr-auto">
                     <Message/>
                     <Contents fixedContents={fixedContents}/>
+                    <Heading text={"ピックアップ団体企画"}/>
                     <iframe className="mb-6" width="100%" height="360px"
                             src={pickupProject.movie} frameBorder="0"
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
