2c63b65a761b581cff3c54d1f7ad0b8e0c86dd7b
山田敬汰 <banira0428@gmail.com>
2020-10-16 20:49:07 +0900

    :add: 岡山大学についての文言を追加

--- pages/president.jsx
+++ pages/president.jsx
@@ -1,4 +1,5 @@
 import Layout from "../components/Layout";
+import changeToUrl from "../lib/regex";
 
 export default function President() {
     return (
@@ -17,9 +18,7 @@ export default function President() {
                     </div>
                     <div className="p-6">
                         <h2 className="text-blue-600 text-xl font-bold mb-3 font-serif">岡山大学について</h2>
-                        <p className="mb-6">岡山大学について岡山大学について岡山大学について岡山大学について岡山大学について岡山大学について岡山大学について
-                            岡山大学について岡山大学について岡山大学について岡山大学について岡山大学について岡山大学について岡山大学について
-                            岡山大学について岡山大学について岡山大学について岡山大学について岡山大学について岡山大学について岡山大学について</p>
+                        <p className="mb-6" dangerouslySetInnerHTML={{__html: changeToUrl('岡山大学は、11学部8研究科を有し、岡山市中心部の緑豊かな広大なキャンパスで、約2万人の学生、留学生が学んでいます。２０２０年には、医学部が創立１５０周年を迎え、２０２１年４月には、工学部と環境理工学部が統合して、新生工学部に生まれ変わります。また、本学は、SDGsを推進しており、学内のみならず地域社会と一体になった取り組みも進めています。正課外活動を行う団体は、文化系の校友会が35団体、体育系の校友会が49団体、医療系地区等の校友会が42団体あるほか、同好会（サークル）も54団体あり、幅広い活動を行っています。詳細はこちらの公式HPをご覧ください https://www.okayama-u.ac.jp/tp/profile/index.html')}}/>
                         <ul className="grid grid-cols-2">
                             {[...Array(9)].map((_,i) =>
                                 <li className="p-3" key={i}>
