52d8068f3ebf939f2922653037826b0c893b3d83
山田敬汰 <banira0428@gmail.com>
2020-10-17 16:49:25 +0900

    :add: ギターマンドリンクラブ用個別ページの雛形を作成

--- pages/clubs.jsx
+++ pages/clubs.jsx
@@ -52,7 +52,7 @@ export default function Clubs({clubs, customs, categories}) {
 
 export async function getStaticProps() {
     const clubs = (await getClubs());
-    const customs = ['okadaiart','pokemon_club','taekwondo','darken','oucrc'];
+    const customs = ['okadaiart','pokemon_club','taekwondo','darken','oucrc','ogmc'];
 
     return {
         props: {
---
+++ pages/clubs/ogmc.jsx
@@ -0,0 +1,63 @@
+import Layout from '../../components/Layout'
+import {getClubWithTitle} from "../../lib/clubs";
+import changeToUrl from "../../lib/regex";
+
+export default function Club({club}) {
+    return (
+        <Layout>
+
+            <div className="w-full">
+                <div className="ml-auto mr-auto max-w-3xl">
+                    <div className="relative mb-6">
+                        <div className="z-10 absolute w-3/4 bg-blue-600 h-full"/>
+                        <div className="z-20 relative content-center p-6 ml-auto mr-auto ">
+                            <h1 className="text-xl text-white mb-12 mt-12 w-3/4">{club.title}</h1>
+                            <img className="object-cover mb-6"
+                                 src={club.main_image ? 'https://drive.google.com/uc?export=view&id=' + club.main_image + '&usp=sharing' : "/1080x720.png"}
+                                 alt="サークルの画像"/>
+                        </div>
+                    </div>
+                    <div className="p-6">
+                        <h2 className="text-blue-600 text-xl font-bold mb-3">この団体について</h2>
+                        <p className="mb-6 break-all"
+                           dangerouslySetInnerHTML={{__html: changeToUrl(club.description)}}/>
+                    </div>
+
+                    {/* この部分を独自のレイアウトで実装する */}
+
+                    {club.movie &&
+                    <div className="p-6">
+                        <h2 className="text-blue-600 text-xl font-bold mb-3">紹介動画</h2>
+                        <iframe className="mb-6" width="100%" height="360px"
+                                src={'https://www.youtube.com/embed/' + club.movie}
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen/>
+                    </div>
+                    }
+                    {club.sub_images &&
+                    <div className="p-6">
+                        <h2 className="text-blue-600 text-xl font-bold mb-3">アルバム</h2>
+                        <ul className="grid grid-cols-2">
+                            {club.sub_images.split(', ').map((it,i) =>
+                                <li className="p-3" key={i}>
+                                    <img src={'https://drive.google.com/uc?export=view&id=' + it + '&usp=sharing' } alt=""/>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                    }
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export async function getStaticProps() {
+    const club = await getClubWithTitle('ogmc');
+    return {
+        props: {
+            club: club
+        }
+    }
+}
