eb0d1fff6a597ee64c6080aaaf2e1c178ef97879
山田敬汰 <banira0428@gmail.com>
2020-10-17 13:12:29 +0900

    :add: 美術部の個別ページ用の画像を追加

--- pages/clubs/okadaiart.jsx
+++ pages/clubs/okadaiart.jsx
@@ -1,9 +1,42 @@
-import Layout from "../../components/Layout";
+import Layout from '../../components/Layout'
+import {getClubWithTitle} from "../../lib/clubs";
+import changeToUrl from "../../lib/regex";
 
-export default function OkadaiArt() {
+export default function Club({club}) {
     return (
         <Layout>
 
+            <div className="w-full">
+                <div className="ml-auto mr-auto max-w-3xl">
+                    <div className="relative mb-6">
+                        <div className="z-10 absolute w-3/4 bg-blue-600 h-full"/>
+                        <div className="z-20 relative content-center p-6 ml-auto mr-auto ">
+                            <h1 className="text-xl text-white mb-12 mt-12 w-3/4">{club.title}</h1>
+                            <img className="object-cover mb-6"
+                                 src={club.main_image ? 'https://drive.google.com/uc?export=view&id=' + club.main_image + '&usp=sharing' : "/1080x720.png"}
+                                 alt="サークルの画像"/>
+                        </div>
+                    </div>
+                    <div className="p-6">
+                        <h2 className="text-blue-600 text-xl font-bold mb-3">この団体について</h2>
+                        <p className="mb-6 break-all"
+                           dangerouslySetInnerHTML={{__html: changeToUrl(club.description)}}/>
+                    </div>
+
+
+                    {/*  作品展がここに入ります  */}
+
+                </div>
+            </div>
         </Layout>
     )
+}
+
+export async function getStaticProps() {
+    const club = await getClubWithTitle('okadaiart');
+    return {
+        props: {
+            club: club
+        }
+    }
 }
