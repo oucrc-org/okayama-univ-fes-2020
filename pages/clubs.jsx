0d82eeaf68980b3425865fce9a20d3a42b598009
山田敬汰 <banira0428@gmail.com>
2020-10-17 14:53:52 +0900

    :add: ダーウィン研究会 個別対応

--- pages/clubs.jsx
+++ pages/clubs.jsx
@@ -52,7 +52,7 @@ export default function Clubs({clubs, customs, categories}) {
 
 export async function getStaticProps() {
     const clubs = (await getClubs());
-    const customs = ['okadaiart','pokemon_club','taekwondo'];
+    const customs = ['okadaiart','pokemon_club','taekwondo','darken'];
 
     return {
         props: {
