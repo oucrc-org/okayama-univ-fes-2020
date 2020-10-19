747d2834d6b9cb234f137379eff6d325084b0d86
山田敬汰 <banira0428@gmail.com>
2020-10-19 16:11:26 +0900

    :add: loading lazy

--- components/Club.jsx
+++ components/Club.jsx
@@ -1,7 +1,7 @@
 export default function Content({club}) {
     return (
         <li className="rounded shadow-md m-2 border-2 p-2">
-            <img className=" rounded mb-1 h-32 object-contain mx-auto" src={club.logo ? 'https://drive.google.com/uc?export=view&id=' + club.logo + '&usp=sharing' : "/1080x720.png"} alt=""/>
+            <img className=" rounded mb-1 h-32 object-contain mx-auto" src={club.logo ? 'https://drive.google.com/uc?export=view&id=' + club.logo + '&usp=sharing' : "/1080x720.png"} alt="" loading="lazy"/>
             <div className="m-3 flex justify-center">
                 <p className="text-sm h-10">{club.title}</p>
             </div>
