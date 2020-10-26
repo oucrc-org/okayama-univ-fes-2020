8d6842d0c83f490fe0f4dc6e2fd99a2ac490594e
a-msy <m5s2y5a@gmail.com>
2020-10-27 00:47:24 +0900

    2kaisei done

--- pages/clubs/oucrc.jsx
+++ pages/clubs/oucrc.jsx
@@ -9,7 +9,7 @@ export default function Club({club}) {
             <div className={"relative eyecatch p-20"}>
                 <div className={"border-4 w-auto h-full rounded border-rainbow"}></div>
                 <img src="/logo_oucrc.png"
-                     className={"absolute m-auto top-0 bottom-0 left-0 right-0 bg-white px-20 py-4"}/>
+                     className={"absolute m-auto top-0 bottom-0 left-0 right-0 bg-white px-20 py-4"} alt={"oucrcのロゴ"}/>
             </div>
             <div className={"center-pc mb-20 mt-20 pt-20 pb-20 relative px-8"}>
                 <img src={"/oucrc/amiga_500.png"}
@@ -26,13 +26,13 @@ export default function Club({club}) {
             <div className={"center-pc px-4 pt-20 pb-20 bushi"}>
                 <h2 className={"h2 text-blue-900 bg-white p-4 mb-20 mt-20"}>過去に配布した部誌、の表紙。</h2>
                 <div className={"flex flex-wrap flex-row max max-w-screen-md m-auto"}>
-                    <div className={"w-1/2 mb-20 pr-4"}>
-                        <img src={"/oucrc/bushi_c96.png"}/>
+                    <div className={"w-1/2 mb-20 pr-4 text-center font-bold"}>
+                        <img src={"/oucrc/bushi_c96.png"} alt={"c96表紙"}/>
                         <p className={"bg-white mt-4 p-4"}>C96</p>
                     </div>
-                    <div className={"w-1/2 mt-20 pl-4"}>
+                    <div className={"w-1/2 mt-20 pl-4 text-center font-bold"}>
                         <p className={"bg-white mb-4 p-4"}>C97</p>
-                        <img src={"/oucrc/IMG_20180715_0001.jpg"}/>
+                        <img src={"/oucrc/IMG_20180715_0001.jpg"} alt={"c97表紙"}/>
                     </div>
                 </div>
             </div>
@@ -58,17 +58,20 @@ export default function Club({club}) {
                                 </li>
                                 <li className={"p-4"}><span className={"text-blue-400"}>次回へ：</span>遠回りなコードをよく書いていたので、もう少しスリムなコードをかけるようにしたい。
                                 </li>
+                                <li>
+                                    <a href={"https://play.google.com/store/apps/details?id=com.wsr.shopping_friend"} className={"text-red-500 p-4"}>ダウンロードはコチラから</a>
+                                </li>
                             </ul>
                         </div>
                         <div className={"sm:w-full md:w-1/2 m-auto"}>
-                            <img src={"/oucrc/amiga_500.png"} className={"m-auto"}/>
+                            <img src={"/oucrc/amiga_500.png"} className={"m-auto"} alt={"買い物の友"}/>
                         </div>
                     </div>
                 </div>
             </div>
             <div className={"bg-gray-200 d2"}>
                 <h2 className={"center-pc h2 text-gray-800 py-20 px-4"}>部員、2Dの作品<br/>びゅーーーん</h2>
-                <div className={"flex flex-wrap"}>
+                <div className={"flex flex-wrap mb-12"}>
                     <div className={"sm:w-full md:w-1/2 md:p-20"}>
                         <h3 className={"h3 px-8 py-4 text-gray-800 bg-white"}>ミニチュアサイズのロボットハンド</h3>
                         <ul className={"font-bold p-8"}>
@@ -86,10 +89,10 @@ export default function Club({club}) {
                         </ul>
                     </div>
                     <div className={"sm:w-full md:w-1/2"}>
-                        <img src={"/oucrc/2d/1.jpg"} className={"img-clip"}/>
+                        <img src={"/oucrc/2d/1.jpg"} className={"img-clip"} alt={"ミニチュアサイズのロボットハンド"}/>
                     </div>
                 </div>
-                <div className={"flex flex-wrap"}>
+                <div className={"flex flex-wrap mb-12"}>
                     <div className={"sm:w-full md:w-1/2 md:p-20"}>
                         <h3 className={"h3 px-8 py-4 text-gray-800 bg-white"}>ペルチェ素子を用いた冷却装置の実験</h3>
                         <ul className={"font-bold p-8"}>
@@ -108,10 +111,10 @@ export default function Club({club}) {
                         </ul>
                     </div>
                     <div className={"sm:w-full md:w-1/2"}>
-                        <img src={"/oucrc/2d/4.jpg"} className={"img-clip"}/>
+                        <img src={"/oucrc/2d/4.jpg"} className={"img-clip"} alt={"ペルチェ素子を用いた冷却装置の実験"}/>
                     </div>
                 </div>
-                <div className={"flex flex-wrap"}>
+                <div className={"flex flex-wrap mb-12"}>
                     <div className={"sm:w-full md:w-1/2 md:p-20"}>
                         <h3 className={"h3 px-8 py-4 text-gray-800 bg-white"}>3Dプリンタを用いたギアボックス</h3>
                         <ul className={"font-bold p-8"}>
@@ -129,7 +132,7 @@ export default function Club({club}) {
                         </ul>
                     </div>
                     <div className={"sm:w-full md:w-1/2"}>
-                        <img src={"/oucrc/2d/5.jpg"} className={"img-clip"}/>
+                        <img src={"/oucrc/2d/5.jpg"} className={"img-clip"} alt={"3Dプリンタを用いたギアボックス"}/>
                     </div>
                 </div>
                 <div className={"flex flex-wrap mb-12"}>
@@ -153,10 +156,10 @@ export default function Club({club}) {
                         </ul>
                     </div>
                     <div className={"sm:w-full md:w-1/2"}>
-                        <img src={"/oucrc/2d/7.jpg"} className={"img-clip"}/>
+                        <img src={"/oucrc/2d/7.jpg"} className={"img-clip"} alt={"パワーLED用の定電流回路（1基板上に容量1Wが3回路)間欠点灯機能も付けてるから省エネ"}/>
                     </div>
                 </div>
-                <div className={"flex flex-wrap"}>
+                <div className={"flex flex-wrap mb-12"}>
                     <div className={"sm:w-full md:w-1/2 md:p-20"}>
                         <h3 className={"h3 px-8 py-4 text-gray-800 bg-white"}>カイワレ大根の水耕栽培装置</h3>
                         <ul className={"font-bold p-8"}>
@@ -175,10 +178,10 @@ export default function Club({club}) {
                         </ul>
                     </div>
                     <div className={"sm:w-full md:w-1/2"}>
-                        <img src={"/oucrc/2d/8.jpg"} className={"img-clip"}/>
+                        <img src={"/oucrc/2d/8.jpg"} className={"img-clip"} alt={"カイワレ大根の水耕栽培装置"}/>
                     </div>
                 </div>
-                <div className={"flex flex-wrap"}>
+                <div className={"flex flex-wrap mb-12"}>
                     <div className={"sm:w-full md:w-1/2 md:p-20"}>
                         <h3 className={"h3 px-8 py-4 text-gray-800 bg-white"}>電界結合方式の無線送電装置</h3>
                         <ul className={"font-bold p-8"}>
@@ -198,13 +201,144 @@ export default function Club({club}) {
                         </ul>
                     </div>
                     <div className={"sm:w-full md:w-1/2"}>
-                        <img src={"/oucrc/2d/9.jpg"} className={"img-clip"}/>
+                        <img src={"/oucrc/2d/9.jpg"} className={"img-clip"} alt={"電界結合方式の無線送電装置"}/>
                     </div>
                 </div>
 
             </div>
-            <div className={"center-pc px-4 pt-20 pb-20 bg-yellow-200 mx-4 my-8"}>
-                <h2 className={"h2 mt-6 mb-6"}>２回生の作品がお披露目です。</h2>
+            <div className={"two_kai py-20 mt-20 text-pink-600"}>
+                <h2 className={"h2 mt-6 mb-6 bg-white px-16"}>２回生の作品がお披露目です。</h2>
+            </div>
+            <div className={"w-full vivace overflow-y-scroll relative"}>
+                <div className={"absolute z-10 w-full px-12 lg:px-48"}>
+                    <h2 className={"h2 mt-24 text-white text-shadow-lightblue"}>VIVACE ヴィヴァーチェ</h2>
+                    <p className={"mt-48 text-bold text-shadow-white"}>
+                        スマートフォンとPC両方でプレイ可能(予定)の音ゲーです。<br/>
+                        降ってきたノーツをタッチパネルかキーボードで叩くよくある感じのアレです。<br/>
+                        <br/>
+                        使用言語はC#(Unity)<br/>
+                        <br/>
+                        Unityでゲームを開発してみたかったのと、せっかくなので部内で作曲をしている人の曲を使いたかったのとでこんな感じの流れになった気がしてます(うろ覚え)。<br/>
+                        <br/>
+                        制作者はいけちぃと智重とふぉです。(楽曲は他にも数名)<br/>
+                    </p>
+                    <p　className={"mt-48 text-bold text-shadow-white mb-16"}>
+                        制作期間は放置してたのも含めると5ヶ月超えてると思います<br/>
+                        <br/>
+                        制作環境は各自VSCodeにGit導入し，Discordでやりとりしてたのでオンラインが主です<br/>
+                        <br/>
+                        Unity初心者しかいなかったのでUnityの仕様を理解するのが一番しんどかったです。<br/>
+                        まだちゃんと動いてないのもそれが原因<br/>
+                        <br/>
+                        感想ofふぉ：スコアの加点を決める式作るの面白くて好きだった<br/>
+                        <br/>
+                        <br/>
+                        曲、もーちょい増えたり変更されたりします
+                    </p>
+                </div>
+                <img className={"w-full h-full object-cover opacity-50 sticky top-0"} src={"/oucrc/two/1.jpg"} alt={"vivace"}/>
+            </div>
+            <div className={"two_kai py-20 bg-black"}>
+                <h3 className={"h3 text-white text-shadow-black mx-8 text-center"}>楽曲リスト(一部抜粋)</h3>
+                <div className={"overflow-x-auto"}>
+                    <table className="bg-black table-fixed text-white m-auto w-980px">
+                        <thead>
+                        <tr>
+                            <th className="px-4 py-2">Title</th>
+                            <th className="px-4 py-2">作曲者</th>
+                            <th className="px-4 py-2">ジャンル</th>
+                            <th className="px-4 py-2">コメント</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td className="border px-4 py-2">Kyubi</td>
+                            <td className="border px-4 py-2">pho</td>
+                            <td className="border px-4 py-2">Hardcore</td>
+                            <td className="border px-4 py-2">九尾の狐です。強そう。	</td>
+                        </tr>
+                        <tr>
+                            <td className="border px-4 py-2">反転孤独線</td>
+                            <td className="border px-4 py-2">pho feat. 重音テト</td>
+                            <td className="border px-4 py-2">J-pop</td>
+                            <td className="border px-4 py-2">窓際、色褪せた一枚の絵に、もう二度と戻れはしないとしても。</td>
+                        </tr>
+                        <tr>
+                            <td className="border px-4 py-2">淡色</td>
+                            <td className="border px-4 py-2">pho feat. 重音テト</td>
+                            <td className="border px-4 py-2">J-pop</td>
+                            <td className="border px-4 py-2">きっと次の春に、また。	</td>
+                        </tr>
+                        <tr>
+                            <td className="border px-4 py-2">時計塔</td>
+                            <td className="border px-4 py-2">智重</td>
+                            <td className="border px-4 py-2">Free style</td>
+                            <td className="border px-4 py-2">SC-88Pro Ver.	</td>
+                        </tr>
+                        <tr>
+                            <td className="border px-4 py-2">終点目指して</td>
+                            <td className="border px-4 py-2">智重</td>
+                            <td className="border px-4 py-2">Free style</td>
+                            <td className="border px-4 py-2">SC-88Pro Ver.	</td>
+                        </tr>
+                        <tr>
+                            <td className="border px-4 py-2">終点目指して</td>
+                            <td className="border px-4 py-2">智重</td>
+                            <td className="border px-4 py-2">Free style</td>
+                            <td className="border px-4 py-2">SC-88Pro Ver.	</td>
+                        </tr>
+                        <tr>
+                            <td className="border px-4 py-2">CryStatuS</td>
+                            <td className="border px-4 py-2">根菜</td>
+                            <td className="border px-4 py-2">Hardcore</td>
+                            <td className="border px-4 py-2">タイトルは「クリステーツ」と読みます。早くて速い曲を作ったので楽しんでいただければ。</td>
+                        </tr>
+                        <tr>
+                            <td className="border px-4 py-2">Maiden Voyage</td>
+                            <td className="border px-4 py-2">Kade</td>
+                            <td className="border px-4 py-2">Modern Jazz Rock</td>
+                            <td className="border px-4 py-2">初めて曲を作りました。ジャズロックはいいぞ。</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className={"video mt-12"}>
+                    <iframe width="640" height="360" src="https://www.youtube.com/embed/pBayNdStr0M" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen></iframe>
+                </div>
+                <h3 className={"h3 text-white text-shadow-black mx-8 my-12"}>ゲームイメージ画像</h3>
+                <div className={"flex flex-wrap mb-12"}>
+                    <div className={"sm:w-full md:w-1/2 m-auto p-4"}>
+                        <img src={"/oucrc/two/2.jpg"} alt={"vivace2"}/>
+                    </div>
+                    <div className={"sm:w-full md:w-1/2 m-auto p-4"}>
+                        <img src={"/oucrc/two/3.jpg"} alt={"vivace3"}/>
+                    </div>
+                </div>
+                <div className={"bg-white py-12"}>
+                    <h3 className={"h3 text-pink-600 mx-8 mb-12"}>別のゲームも製作中！？</h3>
+                    <div className={"flex flex-wrap"}>
+                        <div className={"sm:w-full md:w-1/2 m-auto p-4"}>
+                            <img src={"/oucrc/two/4.png"} alt={"game1"}/>
+                        </div>
+                        <div className={"sm:w-full md:w-1/2 m-auto p-4"}>
+                            <img src={"/oucrc/two/5.png"} alt={"game2"}/>
+                        </div>
+                        <div className={"sm:w-full md:w-1/2 m-auto p-4"}>
+                            <img src={"/oucrc/two/6.jpg"} alt={"game3"}/>
+                        </div>
+                        <div className={"sm:w-full md:w-1/2 m-auto p-4"}>
+                            <img src={"/oucrc/two/7.jpg"} alt={"game4"}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={"py-20 my-20 center-pc"}>
+                <h2 className={"h2 px-4 py-8"}>OUCRCに興味をお持ちですか？<br/>公式サイトもおすすめです。</h2>
+                <a href={"https://oucrc.net"}>
+                    <img src="/logo_oucrc.png" className={"m-auto"} alt={"oucrcのロゴ"}/>
+                </a>
             </div>
         </Layout>
     );
